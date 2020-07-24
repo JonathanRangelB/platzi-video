43ec4353c71db3b07a5824f24d2ee1324d7b9e00
Jonathan Rangel <jonathanrangelb@gmail.com>
2020-07-24 16:24:20 -0500

    Creando la base del proyecto con la transformacion de una pagina estatica a una pagina hecha con componentes en React

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../assets/styles/components/Header.css";
+import PlatziLogo from "../assets/images/logo-platzi-video-BW2.png";
+import ProfileImage from "../assets/images/user-icon.png";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <img className="header__img" src={PlatziLogo} alt="Platzi Video" />
+        <div className="header__menu">
+          <div className="header__menu--profile">
+            <img src={ProfileImage} alt="ProfileImage" />
+            <p>Perfil</p>
+          </div>
+          <ul>
+            <li>
+              <a href="/">Cuenta</a>
+            </li>
+            <li>
+              <a href="/">Cerrar Sesión</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/containers/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
+import "../assets/styles/App.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Search />
+
+      <Categories title="Mi Lista">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Categories title="Favoritos">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Categories title="Escpecial de Platzi video">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
